c3bebe61e3f46f32ea4af470873403a4f938df09
ralphyhlam <ralphyhlam@uchicago.edu>
2025-01-28 23:10:10 +0000

    moved navbar to application.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,4 +19,37 @@ class PagesController < ApplicationController
     render({ :template => "page_templates/rock"})
   end
 
+  def paper 
+    moves= ["rock", "paper", "scissors"]
+    @computer= moves.sample
+    @outcome= ""
+
+    if @computer=="rock"
+     @outcome= "We won!"
+    elsif @computer=="paper"
+     @outcome= "We tied!"
+    elsif @computer=="scissors"
+     @outcome= "We lost!"
+    end
+
+    render({ :template => "page_templates/paper"})
+
+  end
+
+  def scissors
+    moves= ["rock", "paper", "scissors"]
+    @computer= moves.sample
+    @outcome= ""
+
+    if @computer=="rock"
+     @outcome= "We lost!"
+    elsif @computer=="paper"
+     @outcome= "We won!"
+    elsif @computer=="scissors"
+     @outcome= "We tied!"
+    end
+
+    render({ :template => "page_templates/scissors"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/rock", { :controller => "pages", :action => "rock"})
 
   get("/paper", { :controller => "pages", :action => "paper"})
+
+  get("/scissors", { :controller => "pages", :action => "scissors"})
 end
